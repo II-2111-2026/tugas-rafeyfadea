05ffb566a2da72f0763bd5d154fc8b4239d71a21
rafeyfadea <rafeyfa.dea@gmail.com>
2026-04-13 15:17:21 +0700

    submit: W09

--- submissions/w09/answers.py
+++ submissions/w09/answers.py
@@ -13,15 +13,18 @@ from __future__ import annotations
 def q01() -> bool:
     """[T/F] Jika korelasi antara dua variabel adalah nol, maka kedua variabel tersebut pasti
 independen."""
+    return False
     raise NotImplementedError
 
 def q02() -> bool:
     """[T/F] PDF marginal () didapatkan dengan mengintegralkan (,) terhadap dari − ke .
 X"""
+    return True
     raise NotImplementedError
 
 def q03() -> bool:
     """[T/F] Nilai koefisien korelasi selalu berada di antara -1 dan 1."""
+    return True
     raise NotImplementedError
 
 def q04() -> str:
@@ -31,6 +34,7 @@ A) E[X]+E
 B) E[X]E
 C) E[X]/E
 D) 0"""
+    return 'B'
     raise NotImplementedError
 
 def q05() -> str:
@@ -41,6 +45,7 @@ A) Variansi.
 B) Mean.
 C) Korelasi.
 D) CDF."""
+    return 'C'
     raise NotImplementedError
 
 def q06() -> str:
@@ -50,6 +55,7 @@ A) 13
 B) 5
 C) 36
 D) 6,5"""
+    return 'A'
     raise NotImplementedError
 
 def q07() -> str:
@@ -59,26 +65,32 @@ A) (,)/ () X
 B) (,)/()
 C) () () X
 D) (,)− () X"""
+    return 'A'
     raise NotImplementedError
 
 def q08() -> float:
     """[Numeric] Jika (X,) = 2, X = 2, = 2, berapakah koefisien korelasinya?"""
+    return 0.5
     raise NotImplementedError
 
 def q09() -> float:
     """[Numeric] Berapakah nilai E jika E[X] = 10 dan E = 20?"""
+    return 30.0
     raise NotImplementedError
 
 def q10() -> float:
     """[Numeric] Jika (,) = 1/4 untuk 0 2 dan 0 2, berapakah P(X 1, 1)?"""
+    return 0.25
     raise NotImplementedError
 
 def q11() -> float:
     """[Numeric] Dalam tabel diskrit, jika P(1,1) = 0,1P(1,2) = 0,2P(2,1) = 0,3P(2,2) = 0,4,
 berapakah probabilitas marginal P(X = 1)?"""
+    return 0.3
     raise NotImplementedError
 
 def q12() -> float:
     """[Numeric] Jika X dan memiliki korelasi 1 dan (X) = 4, berapakah (X+X)?"""
+    return 16.0
     raise NotImplementedError
 
