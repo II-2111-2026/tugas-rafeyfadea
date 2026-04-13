44b7ec6aacab9bf0a653bca9c59062c3f08fa3f0
rafeyfadea <rafeyfa.dea@gmail.com>
2026-04-13 15:45:13 +0700

    submit: w13

--- submissions/w13/answers.py
+++ submissions/w13/answers.py
@@ -12,6 +12,7 @@ Format jawaban:
 from __future__ import annotations
 def q01() -> bool:
     """[T/F] Garis regresi meminimalkan jumlah total selisih absolut antara data dan garis."""
+    return False
     raise NotImplementedError
 
 def q02() -> bool:
@@ -19,11 +20,13 @@ def q02() -> bool:
 R2
 sebesar 0,85 berarti 85% variasi pada variabel dependen dapat dijelaskan
 oleh variabel independen."""
+    return True
     raise NotImplementedError
 
 def q03() -> bool:
     """[T/F] Regresi linear berganda menggunakan lebih dari satu variabel independen untuk
 memprediksi satu variabel dependen."""
+    return True
     raise NotImplementedError
 
 def q04() -> str:
@@ -33,6 +36,7 @@ A) Intersep.
 B) Kemiringan (slope).
 C) Varians.
 D) Galat."""
+    return 'B'
     raise NotImplementedError
 
 def q05() -> str:
@@ -42,6 +46,7 @@ A) Sangat lemah.
 B) Sangat kuat dan negatif.
 C) Tidak ada hubungan.
 D) Positif."""
+    return 'B'
     raise NotImplementedError
 
 def q06() -> str:
@@ -53,6 +58,7 @@ A) 0,99
 B) 0,50
 C) 0,00
 D) -1,00"""
+    return 'C'
     raise NotImplementedError
 
 def q07() -> str:
@@ -62,26 +68,32 @@ A) Slope.
 B) Intersep.
 C) Origin.
 D) Outlier."""
+    return 'B'
     raise NotImplementedError
 
 def q08() -> float:
     """[Numeric] Jika persamaan regresi adalah = 5+2X, berapakah nilai prediksi untuk
 X = 10?"""
+    return 25.0
     raise NotImplementedError
 
 def q09() -> float:
     """[Numeric] Jika R2 = 0,64, berapakah nilai korelasi (ambil nilai positif)?"""
+    return 0.8
     raise NotImplementedError
 
 def q10() -> float:
     """[Numeric] Berapakah nilai rata-rata dari residual pada model regresi linear klasik?"""
+    return 0.0
     raise NotImplementedError
 
 def q11() -> float:
     """[Numeric] Jika naik 10 unit ketika X naik 2 unit, berapakah nilai slope ?"""
+    return 5.0
     raise NotImplementedError
 
 def q12() -> float:
     """[Numeric] Jika S = 40 dan S = 10, berapakah nilai estimasi slope ?"""
+    return 4.0
     raise NotImplementedError
 
